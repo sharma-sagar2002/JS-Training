12b2f71584cbfb6f8dbd4ab27dda05477e354125
sharma-sagar2002 <sagar2610sharma@gmail.com>
2024-07-16 09:26:53 +0530

    JS basics

---
+++ Day 1/practice.js
@@ -0,0 +1,66 @@
+//syntax and scope for let const and var
+// var a=10;  //global scope or functional scope
+// let b=10; // block scope 
+// const c=10;  // block scope
+
+
+// //primitive data type 
+// let num=10;
+// let str="sagar";
+// let isTrue=true;
+// let n;  //undefined
+// let nb=null; //null
+
+// let s=Symbol("description");  //immutable and always gives a unique value and are used for creating unique value in objects 
+// console.log(s);
+
+
+
+// -----arithmetic operator-------
+// //add
+// a=b+c;
+// //subtract 
+// b=a-c;
+// //exponentiation 
+// b=c**2;  //(c^2)
+
+// //multiply
+// a=a*b;
+// //divide 
+// a=b/a;
+// //modulus
+// b=a%c;
+
+
+//  -------logical operator-------
+//  (a==b)&&(c===a) - return true is both gives true
+//  || - return true if any one gives true
+//  ! - return opposite of true or false 
+
+// ------comparison operator-------
+// a==b equals operator
+// a===b strictly equals 
+// a!=b not equals 
+// a!==b strictly not equals 
+
+// primitive value :
+// -fixed size 
+// - stored in call stack
+// - points to same memory if new variable is = old primtive value type varible 
+
+// reference value 
+// - stored in heap  
+// - having dynamic size  
+// - accessed by refernce or pointer 
+
+// Array creation types 
+//  - using normal syntax 
+//    eg : let a=[];
+//  - using constructor
+//    - let a= new Array(2);
+//    - let a=new Array (1,2,4);
+
+
+     
+
+
